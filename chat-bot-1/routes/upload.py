a3a308ba9c66ec9dee7346a6f4e123edc3431060
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-05-07 00:03:52 +0530

    embedding remains

--- chat-bot-1/routes/upload.py
+++ chat-bot-1/routes/upload.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, File, UploadFile
 import fitz
 import os
-
+from services.pdf import extract_text_from_pdf
 router = APIRouter()
 
 UPLOAD_DIR = "data/uploads"
@@ -15,5 +15,8 @@ async def upload_file(file: UploadFile = File(...)):
     file_path= os.path.join(UPLOAD_DIR, file.filename)
     with open(file_path, "wb") as f:
         f.write(await file.read())
+    text = extract_text_from_pdf(file_path)
+    
     return{"message": f"File '{file.filename}' uploaded successfully.",
-        "file_path": file_path}
+        "file_path": file_path,
+        "text": text[:200]}
