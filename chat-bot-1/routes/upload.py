f7007ec9992576b6a0114011e2f9e3af6ebb7b98
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-05-06 22:55:10 +0530

    trying to dry run

--- chat-bot-1/routes/upload.py
+++ chat-bot-1/routes/upload.py
@@ -7,4 +7,13 @@ router = APIRouter()
 UPLOAD_DIR = "data/uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-@
+@router.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    if file.content_type != "application/pdf":
+        return {"error": "Only PDF files are allowed."}
+
+    file_path= os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+    return{"message": f"File '{file.filename}' uploaded successfully.",
+        "file_path": file_path}
