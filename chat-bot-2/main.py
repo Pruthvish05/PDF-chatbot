5eb9f5585f3669a4349850bedc65f8d8f9def78b
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-05-08 21:13:56 +0530

    Update main.py

--- chat-bot-2/main.py
+++ chat-bot-2/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from routes import upload, query
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI(title="PDF AI Chatbot Backend")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Register Routes
 app.include_router(upload.router, tags=["Upload"])
 app.include_router(query.router, tags=["Query"])
